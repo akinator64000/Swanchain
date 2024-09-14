1045259888a4798a2b6bd02aa4d5206cad7dd89b
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2024-09-14 02:30:46 +0100

    Create Game.sol

---
+++ Contracts/Game.sol
@@ -0,0 +1,4 @@
+// @title GAME token for battledogs Arena
+// https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
+
+pragma solidity ^0.8.17;
