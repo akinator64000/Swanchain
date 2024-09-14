327b7402902c6810512648895d33ba021432ff5b
0xSorcerers <121747420+0xsorcerers@users.noreply.github.com>
2024-09-14 13:29:37 +0100

    Create Battledog.sol

---
+++ Contracts/Battledog.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: No license
+
+
+// @title NFT Game by OxSorcerers for Battledog Games (Neon EVM - Solana)
+// https://twitter.com/0xSorcerers | https://github.com/Dark-Viper | https://t.me/Oxsorcerer | https://t.me/battousainakamoto | https://t.me/darcViper
+
+solidity ^0.8.17;
